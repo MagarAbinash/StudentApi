60a8ceca1f462ffd877d1e9c5647556248a71dc7
Abinash magar <magarabinash986@gmail.com>
2021-01-26 09:22:15 +0545

    Initial commit

---
+++ StudentApi/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace StudentApi.Models
+{
+    public partial class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public long? Contact { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ StudentApi/Models/StudentRecordsContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace StudentApi.Models
+{
+    public partial class StudentRecordsContext : DbContext
+    {
+        public StudentRecordsContext()
+        {
+        }
+
+        public StudentRecordsContext(DbContextOptions<StudentRecordsContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-737OAU2\\ABINASH;Initial Catalog=StudentRecords;Integrated Security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.ToTable("Student");
+
+                entity.HasIndex(e => e.Id, "UQ__Student__3213E83EC43FD617")
+                    .IsUnique();
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Address)
+                    .HasMaxLength(80)
+                    .IsUnicode(false)
+                    .HasColumnName("address");
+
+                entity.Property(e => e.Contact).HasColumnName("contact");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("name");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
